9bc2f2e792e7c4259102ac39e46689bb5b97bc00
NeobisCRMFood <crmfood.project@gmail.com>
2021-01-29 10:58:46 +0400

    Изменения

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 let navWrapper = document.querySelector('.nav-wrapper'),
-    navToogler = document.querySelector('.nav-toogler')
+    navToogler = document.querySelector('.nav-toggler')
 
 navToogler.addEventListener('click', function (event) {
     navWrapper.classList.toggle('active')
